08bb57dc3cf943ea41741c10ea2a08643cf7ccef
M1ngXU <max.mingxu@yahoo.com>
2023-06-20 18:40:35 +0200

    added cuda (maybe)

--- src/from_sparse.rs
+++ src/from_sparse.rs
@@ -52,3 +52,135 @@ impl<T: Tape<f32, Self>, OutputShape: Shape<Concrete = [usize; OutputShape::NUM_
 		output.put_tape(tape)
 	}
 }
+
+#[cfg(feature = "cuda")]
+impl<T: Tape<f32, Self>, OutputShape: Shape<Concrete = [usize; OutputShape::NUM_DIMS]>>
+	FromSparse<f32, T, OutputShape> for Cuda
+{
+	fn from_sparse(
+		&self,
+		values: Tensor<(usize,), f32, Self, T>,
+		indeces: Tensor<(usize, Const<{ OutputShape::NUM_DIMS }>), usize, Self>,
+		output_shape: OutputShape,
+	) -> Tensor<OutputShape, f32, Self, T> {
+		use cudarc::driver::{DeviceRepr, DeviceSlice, LaunchAsync, ValidAsZeroBits};
+
+		assert_eq!(values.shape().0, indeces.shape().0);
+
+		if !self.dev.has_func("from_sparse_f32", "from_sparse_fwd_f32") {
+			self.dev.load_ptx(PTX_SRC.into(), "from_sparse_f32", [
+				"from_sparse_fwd_f32",
+				"from_sparse_bwd_f32",
+			])?;
+		}
+
+		let fwd_fn = self
+			.dev
+			.get_func("from_sparse_f32", "from_sparse_fwd_f32")
+			.unwrap();
+		let bwd_fn = self
+			.dev
+			.get_func("from_sparse_f32", "from_sparse_bwd_f32")
+			.unwrap();
+
+		let (values, mut tape) = values.split_tape();
+
+		let cfg = launch_config::<128>(values.shape().0);
+
+		let mut output = self.zeros_like(&output_shape);
+
+		let mut values_info = Vec::with_capacity(1 * 2);
+		values_info.extend(values.shape().concrete());
+		values_info.extend(values.strides().concrete());
+		let values_info = self.dev.htod_copy(values_info).unwrap();
+
+		let mut indeces_info = Vec::with_capacity(2 * 2);
+		indeces_info.extend(indeces.shape().concrete());
+		indeces_info.extend(indeces.strides().concrete());
+		let indeces_info = self.dev.htod_copy(indeces_info).unwrap();
+
+		let mut output_info = Vec::with_capacity(OutputShape::NUM_DIMS * 2);
+		output_info.extend(output.shape().concrete());
+		output_info.extend(output.strides().concrete());
+		let output_info = self.dev.htod_copy(output_info).unwrap();
+
+		let params = (
+			values.shape().0,
+			&values,
+			&values_info,
+			&indeces,
+			&indeces_info,
+			&mut output,
+			&output_info,
+			OutputShape::NUM_DIMS,
+		);
+		unsafe {
+			fwd_fn.launch(cfg, params).unwrap();
+		}
+
+		let inp_ghost = values.clone();
+		let out_ghost = output.clone();
+		tape.add_backward_op(move |grads| {
+			grads.try_alloc_for(&inp_ghost)?;
+			grads.try_alloc_for(&out_ghost)?;
+			let (grad_inp, grad_out) = grads.mut_and_ref(&inp_ghost, &out_ghost);
+			let params = (
+				values.shape().0,
+				&mut grad_inp,
+				&values_info,
+				&indeces,
+				&indeces_info,
+				&grad_out,
+				&output_info,
+				OutputShape::NUM_DIMS,
+			);
+			unsafe {
+				bwd_fn.launch(cfg, params).unwrap();
+			}
+			Ok(())
+		});
+		output.put_tape(tape)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn from_sparse() {
+		let dev = AutoDevice::default();
+
+		let indeces = dev
+			.tensor([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4]])
+			.reshape_like(&(5, Const::<2>));
+		let values = dev.tensor([0.0, 1.0, 2.0, 3.0, 4.0]).reshape_like(&(5,));
+
+		let (output, mut tape) = dev
+			.from_sparse(values.clone().leaky_traced(), indeces, (5, 5))
+			.split_tape();
+
+		let expected = vec![
+			0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 0.0, 0.0, 0.0, 0.0,
+			0.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0,
+		];
+
+		assert_eq!(output.as_vec(), expected);
+
+		for (i, g) in tape
+			.gradients
+			.get_or_alloc_mut(&output)
+			.unwrap()
+			.iter_mut()
+			.enumerate()
+		{
+			*g = i as f32;
+		}
+
+		let grads = tape.execute().unwrap();
+
+		let gradients = grads.get(&values);
+		let expected = vec![0.0, 6.0, 12.0, 18.0, 24.0];
+		assert_eq!(gradients.as_vec(), expected);
+	}
+}
